f99014bd44eb904618a2af17ba71834981445c46
nour <nour.fwh@gmail.com>
2013-04-11 12:33:44 +0300

    add deal hands for 2 players

--- script/custom.js
+++ script/custom.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     var self = this,
       suits = ["spades", "hearts", "diamonds", "clubs", "joker"], 
       ranks = [, "ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"],
-      Card  = function(suit, rank) {
+      Card  = function(id, suit, rank) {
+        this.id = id;
         this.suit = suit;
         this.rank = rank;
         this.name = function() {
@@ -23,7 +24,6 @@ $(document).ready(function () {
       },
       Deck = function() {
         this.cards = [];
-        this.cardOnTopId;
         this.init();
       }, 
       Player = function(name, game) {
@@ -45,7 +45,7 @@ $(document).ready(function () {
         var i = 0, self = this;
         for (var suit=0; suit<=3; suit++) {
           for (var rank=1; rank<=13; rank++) {
-            this.cards[i] = new Card(suit, rank); i++;
+            this.cards[i] = new Card(i, suit, rank); i++;
           }
         }
       },
@@ -55,7 +55,6 @@ $(document).ready(function () {
           var j = Math.floor(Math.random() * (i + 1));
           this.cards.swap(i, j);
         }
-        this.cardOnTopId = this.cards.length-1;
       },
       deal: function() {
         return this.dealHand(1)[0];
@@ -69,32 +68,6 @@ $(document).ready(function () {
       },
       cardsLeft: function() {
         return this.cards.length;
-      },
-      moveCardOnTop: function() {
-        if (this.cardOnTopId < 0) return;
-        var self = this, $cardOnTop = $('img[data-card-id='+ this.cardOnTopId+']');
-        $cardOnTop.animate({
-          left: '+=120',
-          top: '125px',
-        }, 200);
-        $cardOnTop.attr('src', self.cards[self.cardOnTopId].url(true)); 
-        $cardOnTop.css('z-index', $cardOnTop.css('z-index')*-1);
-        this.cardOnTopId -= 1;
-      },
-      renderFirstTime: function() {
-        var html = '', top = 125, left = 190, zindex = -52, self=this;
-        for(var i=0; i<this.cards.length; i++){
-          html+='<img src="'+this.cards[i].url(false)
-          +'" style="top:'+top+'px; left:'+left+'px; z-index:'+zindex
-          +';" data-card-id="'+i+'" />';
-          if (i%7 === 0) { top -= 1; left -= 1; }
-          zindex += 1;
-        }
-        $("#card-table").html(html);
-        $('img[data-card-id]').click(function(){
-          self.moveCardOnTop();
-          $(this).unbind('click');
-        });
       }
     };
     Player.prototype = {
@@ -120,37 +93,12 @@ $(document).ready(function () {
         turn: null,
         deck: new Deck(),
         init: function(player1, player2) {
-          // console.log(this);
           this.players.push(new Player(player1, this));
           this.players.push(new Player(player2, this));
           this.turn = this.players[0];
           this.deck.shuffle();
-          // this.players[0].hand = this.deck.dealHand(2);
-          // this.players[1].hand = this.deck.dealHand(2);
-
-          // graphics
-          // //Start by initalizing the library
-          // cards.init({table:'#card-table'});
-          // //Create a new deck of cards
-          // deck = new cards.Deck(); 
-          this.deck.renderFirstTime();
-
-          
-          // // //cards.all contains all cards, put them all in the deck
-          // deck.addCards(cards.all); 
-          // // //No animation here, just get the deck onto the table.
-          // deck.render({immediate:true});
-
-          // // //Now lets create a couple of hands, one face down, one face up.
-          // upperhand = new cards.Hand({faceUp:false, y:50});
-          // lowerhand = new cards.Hand({faceUp:true, y:350});
-          // //Deck has a built in method to deal to hands.
-          // deck.deal(2, [upperhand, lowerhand], 50)
-
-
         },
         start: function() {
-          // this.deck.view()
           // this.turn.play();
         },
         switchTurns: function() {
@@ -158,104 +106,117 @@ $(document).ready(function () {
         },
         playCard: function(card) {
           console.log(this.turn.name + " just played: " + card.toString());
-          // var card = this.turn.play();
-          // display card
           this.switchTurns();
           this.turn.play();
+        },
+        dealHandsToPlayers:function (numOfCards){
+          if (numOfCards * this.players.length > this.deck.cards.length) {
+            console.log("# of cards in deck is not enough to deal hands to players");
+            return false;
+          } else {
+            for(var i=0; i<this.players.length; i++){
+              this.players[i].hand = this.deck.dealHand(numOfCards);
+            }
+            return true;
+          }
         }
       }
     };
   }();
-  var renderCards = function(){
+  var renderCards = function(game){
+    return {
+      game: game,
+      cards: game.deck.cards,
+      cardOnTopId: game.deck.cards.length - 1,
+      players: game.players,
+      cardsCss: {
+        height: 130,
+        width: 100
+      },
+      createCard: function(options){
+        return $('<img />').attr({
+          src:options['src'],
+          'data-card-id':options['data-card-id']
+        }).css({
+          'left':options['left']+'px',
+          'top':options['top']+'px',
+          'z-index':options['zindex']
+        });
+      },
+      renderDeck: function() {
+        var self = this, html = "", deck = $('.deck'), zindex = -52,
+        top = 170, left = 170;
 
-  }();
-  // var game = cards();
-  // console.log(serial_maker.gensym())
-  // var card = game.newCard(0, 0);
-  // var card = game.newCard(0, 0);
-  // var deck = game.newDeck();
-  // console.log( cards);
-  // console.log( cards.newCard(1, 2).toString());
-  // console.log( cards.newCard(2, 3).toString() );
-  // var deck = cards.newDeck();
-  // console.log( deck.sample );
-  // deck.shuffle();
-  // console.log(deck.dealHand(5));
-  // console.log(deck.deal());
-  // console.log(deck.cards);
+        for(var i=0; i<this.cards.length; i++){
+          html+= this.createCard({
+            src: this.cards[i].url(false),
+            top: top, left: left, zindex: zindex,
+            'data-card-id': this.cards[i].id
+          })[0].outerHTML;
+          if (i%7 === 0) { top -= 1; left -= 1; }
+          zindex += 1;
+        }
+        $('.deck').html(html);
+      },
+      makeDeckClickable: function() {
+        var self = this;
+        $('img[data-card-id]').click(function(){
+          self.moveCardOnTop();
+          $(this).unbind('click');
+        });
+      },
+      moveCardOnTop: function() {
+        if (this.cardOnTopId < 0) return;
+        var self = this, $cardOnTop = $('img[data-card-id='+ this.cardOnTopId+']');
+        $cardOnTop.animate({
+          left: '+=120',
+          top: '125px',
+        }, 200);
+        $cardOnTop.attr('src', self.cards[self.cardOnTopId].url(true)); 
+        $cardOnTop.css('z-index', $cardOnTop.css('z-index')*-1);
+        this.cardOnTopId -= 1;
+      },
+      renderPlayers: function(){
+        for(var i=0; i<this.players.length; i++){
+          $('.player'+i+' h3').text(this.players[i].name);          
+        }
+      },
+      renderDealHands: function() {
+        var self = this, html = '', top = 10, left = 200, zindex = -52;
 
-  // var p1 = cards.newPlayer("nour");
-  // var p2 = cards.newPlayer("pal");
+        $("#dealHands").click(function(){
 
-  // p1.hand = deck.dealHand(3);
-  // p2.hand = deck.dealHand(3);
-  // console.log(deck.cardsLeft());
-  // console.log(p1.hand);
-  // console.log(p2.hand);
+          if (mycards.game.dealHandsToPlayers(3)) {
+            for (var i=0; i<self.players.length; i++) {
+              html = "";
+              for(var j=0; j<self.players[i].hand.length; j++) {
+                // get card on top
+                // $('.deck img[data-card-id]').filter(function(index) {
+                  // return $(this).css('z-index') === -1 * this.cards.length;
+                // })
+                html+= self.createCard({
+                  src: self.players[i].hand[j].url(true),
+                  // left: left, top:top, zindex: zindex,
+                  'data-card-id': self.players[i].hand[j].id
+                })[0].outerHTML;
+              }
+              $('.player'+i+' .cards').html(html);
+            }
+            self.renderDeck();
+          }
 
-  $("#p1").click(function(){
-    // p1.play();
-  });
-  $("#p2").click(function() {
-    // p2.play();
-  });
-mycards.game.init("nour", "pal");
-mycards.game.start();
+        });
+      }
+    };
+  }(mycards.game);
 
-
-
-
-
-
-    
-// deck.click(function(card){
-//   if (card === deck.topCard()) {
-//     lowerhand.addCard(deck.topCard());
-//     lowerhand.render();
-//   }
-// });
-
-// deck.x -= 50;
-// deck.render();
-// discardPile = new cards.Deck({faceUp:true});
-// discardPile.x += 50;
-// deck.render({callback:function() {
-//   discardPile.addCard(deck.topCard());
-//   discardPile.render();
-// }});
   
-//   lowerhand.click(function(card){
-//   if (card.suit == discardPile.topCard().suit 
-//     || card.rank == discardPile.topCard().rank) {
-//     discardPile.addCard(card);
-//     discardPile.render();
-//     //lowerhand.render();
-//   }
-// });
+mycards.game.init("nour", "pal");
+mycards.game.dealHandsToPlayers(3);
+// mycards.game.start();
 
-
-  // console.log(  )
-  // console.log( cards.game.deck )
-  // console.log(deck.deal().toString());
-  // console.log(deck.cardsLeft());
-
-  // deck.changeDeck();
-  // var deck2 = cards.newDeck();
-  // console.log( deck.sample );
-  // console.log( deck2.cards );
-  // console.log( cards.newDeck().init() );
-  // console.log( cards.newDeck().changeDeck() );
-  // console.log( cards.newDeck().getDeck() );
-  // console.log( deck.cards );
-  // console.log(new cards.Card(0, 1).toString());
-  // console.log(new cards.Card(0, 0).legalCard());
-
+renderCards.renderDeck();
+// renderCards.makeDeckClickable();
+renderCards.renderPlayers();
+renderCards.renderDealHands();
 });
-      // this.print = function() {
-      //   for(var i=0; i<suits.length; i++) {
-      //     console.log(suits[i]);
-      //   }
-      //   for(var i=0; i<ranks.length; i++) {
-      //     console.log(ranks[i]);
-      //   }
-      // }
